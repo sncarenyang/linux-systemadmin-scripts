2cac6a8704db7fa77a374b6bbc0e7c93c059c6b5
Shi-Ning Caren Yang <caren.yang@gmail.com>
2026-04-30 21:04:56 +0800

    Update cpu-mem-monitor.sh

--- cpu-mem-monitor.sh
+++ cpu-mem-monitor.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # CPU/Mem monitor
 
+# bc debug 
+if ! command -v bc &> /dev/null
+then
+    echo "Error: bc is not installed. Please install it first."
+    exit 1
+fi
+
 CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\\([0-9.]*\\)%* id.*/\1/" | awk '{print 100-$1"%"}')
 MEM=$(free | grep Mem | awk '{printf "%.1f%%", $3/$2 * 100.0}')
 
